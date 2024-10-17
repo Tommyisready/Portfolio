62d44a5f25365d26061fec79bbc6c2046f1f52b9
Tommyisready <perrot.thomas@live.fr>
2024-10-17 15:19:39 +0200

    done

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ const Home = () => {
             {/* button and social */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <a href="public/cv-thomas-perrot.pdf" download>
+                <a href="/cv-thomas-perrot.pdf" download>
                   <span>Télécharger CV</span>
                   <FiDownload className="text-xl" />
                 </a>
